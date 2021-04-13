5e0f68977cf77960970998c20b8e314ba824e531
Natalia <natalialllepost@gmail.com>
2021-04-13 16:03:26 +0200

    add form

---
+++ basicapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from . import forms
+
+# Create your views here.
+
+
+def index(req):
+    return render(req, 'index.html')
+
+
+def form_name_view(req):
+    form = forms.FormBasic()
+
+    if req.method == 'POST':
+        form = forms.FormBasic(req.POST)
+
+        if form.is_valid():
+            print("validation success")
+            print("NAME" + form.cleaned_data['name'])
+            print("EMAIL" + form.cleaned_data['email'])
+            print("TEXT" + form.cleaned_data['text'])
+
+    return render(req, 'form.html', {'form': form})
