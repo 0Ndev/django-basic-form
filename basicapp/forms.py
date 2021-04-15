22da7fa60401312115f1c175b11e22b3ae7e0997
Natalia <natalialllepost@gmail.com>
2021-04-15 09:14:41 +0200

    build-in and custom form validation

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -3,20 +3,32 @@ from django.core import validators
 
 
 # build-in-validation
-def check_for_z(value):
-    if value[0].lower() != 'z':
-        raise forms.ValidationError("NAME NEEDS TO START WITH Z")
+# def check_for_z(value):
+#     if value[0].lower() != 'z':
+#         raise forms.ValidationError("NAME NEEDS TO START WITH Z")
 
 
 class FormBasic(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
+    # name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
+    # valid email
+    verify_email = forms.EmailField(label='enter your email again')
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.ChoiceField(required=False,
                                    widget=forms.HiddenInput,
                                    validators=[
                                        validators.MaxLengthValidator(0)]
                                    )
+
+    # custom valid form
+    def clean(self):
+        all_clean_date = super().clean()
+        email = all_clean_date['email']
+        vmail = all_clean_date['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILs MATCH")
 
     # botcatcher
     # def clean_botcatcher(self):
